69ad56e1b5eb767ed52080b859e5994764408c59
Nitesh Shah <niteshshah000@gmail.com>
2024-07-14 19:43:42 +0530

    updates

--- resources/views/frontend/privacy_policy.blade.php
+++ resources/views/frontend/privacy_policy.blade.php
@@ -23,39 +23,88 @@
 <!-- product tab start -->
 <section class="whish-list-section theme1 pb-70">
     <div class="container grid-wraper">
-        <p class="mb-20 text-justify">This Privacy policy describes how ekaavastra.com (the “Site” or “we”) collects, uses, and discloses your Personal Information when you visit or make a purchase from the Site.</p>
-        <h5 class="title mb-20">Contact</h5>
-        <p class="mb-20 text-justify">After reviewing this policy, if you have additional questions, want more information about our privacy practices, or would like to make a complaint, please contact us via Whatsapp on 9636373743 or by mail using the details provided below:
-            Sunshine Aditya, Maharana Pratap Road,Sirsi 302021</p>
-        <h5 class="title mb-20">Collecting Personal Information</h5>
-        <p class="mb-20 text-justify">When you visit the Site, we collect certain information about your device, your interaction with the Site, and information necessary to process your purchases. We may also collect additional information if you contact us for customer support. In this Privacy Policy, we refer to any information about an identifiable individual (including the information below) as “Personal Information”. See the list below for more information about what Personal Information we collect and why.</p>
-        <h6 class="title mb-20">Device Information</h6>
-        <ul>
-            <li><b>Purpose of collection:</b> to load the Site accurately for you, and to perform analytics on Site usage to optimize our Site.</li>
-            <li><b>Source of collection: </b> Collected automatically when you access our Site using cookies, log files, web beacons, tags or pixels.</li>
-            <li><b>Personal Information collected: </b> version of web browser, IP address, time zone, cookie information, what sites or products you view, search terms, and how you interact with the Site.</li>
+        <p class="mb-20 text-justify">The domain name www.ekaavastra.com (“Website”) is a site operated by EKAA VASTRA, a company incorporated under laws of India with our registered office at Sunshine Aditya, Maharana Pratap Road,Sirsi 302021 (“Company/ EKAA VASTRA”).
+        </p>
+        <p class="mb-20 text-justify">Through this document, we wish to make you feel safe and comfortable while using our website. We want to make you fully aware of certain data/information that we will be collecting from you so that you feel confident sharing that particular information with us.</p>
+        <p class="mb-20 text-justify">This Privacy Policy covers the information EKAA VASTRA collects from the user(s) of www.ekaavastra.com ("Website"). By using the services on the www.ekaavastra.com website, you agree to the collection and use of information in relation to this policy, terms of service, conditions, etc.</p>
+        <p class="mb-20 text-justify">Please note-: Our privacy policy is subject to change from time to time without notice. To make sure you are aware of the changes, please review this policy periodically.
+        </p>
+        <h5 class="title mb-20">1. Customer/User Data</h5>
+        <p class="mb-20 text-justify">For an enhanced shopping experience, we may require you to provide us with certain identifiable information about you which may not be limited to name, phone number, and postal address. We use this information to contact or identify you.</p>
+        <p class="mb-20 text-justify">Here are the types of data we may collect and how we may use them-</p>
+        <p class="mb-20 text-justify"><b>Personal details</b></p>
+        <p class="mb-20 text-justify">This will include your-</p>
+        <ul class="mb-20">
+            <li>First and last name</li>
+            <li>Email address</li>
+            <li>Contact number</li>
+            <li>Shipping or billing address</li>
+            <li>Pin code</li>
         </ul>
-        <h6 class="title mb-20 mt-20">Order Information</h6>
-        <ul>
-            <li><b>Purpose of collection:</b> to provide products or services to you to fulfill our contract, to process your payment information, arrange for shipping, and provide you with invoices and/or order confirmations, communicate with you, screen our orders for potential risk or fraud, and when in line with the preferences you have shared with us, provide you with information or advertising relating to our products or services.</li>
-            <li><b>Source of collection: </b> collected from you.</li>
-            <li><b>Personal Information collected: </b> name, billing address, shipping address, payment information (including credit card numbers, UPI,), email address, and phone number.</li>
+        <p class="mb-20 text-justify"><b>Log data-</b></p>
+        <p class="mb-20 text-justify">When you visit our websites, your browser sends us information called as “log data” which includes-</p>
+        <ul class="mb-20">
+            <li>The type of device you use</li>
+            <li>The IP address of your device</li>
+            <li>The type of Internet browser you use</li>
+            <li>The pages you visit/access</li>
+            <li>The links you click on our website</li>
+            <li>The number of times you access the page</li>
+            <li>Things you view, add to bag, add to wishlist</li>
         </ul>
-        <h6 class="title mb-20 mt-20">Customer Support Information</h6>
-        <ul>
-            <li><b>Personal Information collected:</b> name, email address, and phone number.</li>
-            <li><b>Purpose of collection: </b>  to provide customer support.</li>
-            <li><b>Source of collection: </b> collected from you</li>
+        <p class="mb-20 text-justify"><b>Cookies-</b></p>
+        <p class="mb-20 text-justify">Cookies are files with small amount of data that is commonly used as an anonymous unique identifier. These are sent to your browser from the website that you visit and are stored on your computer’s hard drive.</p>
+        <p class="mb-20 text-justify">Our website uses these “cookies” to collect information and to improve our website to give you better shopping experience.</p>
+        <p class="mb-20 text-justify">You have the option to either accept or refuse these cookies, and know when a cookie is being sent to your computer. If you choose to refuse our cookies, you may not be able to use some portions of our website.</p>
+        <p class="mb-20 text-justify"><b>How we use your personal data</b></p>
+        <p class="mb-20 text-justify">In general, we use your personal information to improve our products and services, to communicate with you, to offer you personalized services and to amp up the security of our website. We will be making calls and sending SMS through a third-party platform. With your information We sought your consent to receive promotional / confirmation calls on the number shared, and such calls and SMS would be coming from a third party platform.</p>
+        <p class="mb-20 text-justify"><b>Improving our services-</b></p>
+        <p class="mb-20 text-justify">We use personal data to help us provide, improve, and develop our products, services, and advertising. This includes using personal data for purposes such as data analysis, research, and audits.
+        </p>
+        <p class="mb-20 text-justify"><b>Contacting you-</b></p>
+        <p class="mb-20 text-justify">Subject to your prior expressed consent for using our services, we may use personal data to send you marketing communications in relation to EKAA VASTRA owned products and services, communicate with you about your account or transactions, and inform you about our policies and terms. If you no longer wish to receive email communications for marketing purposes, please contact us to opt-out anytime you want.</p>
+        <p class="mb-20 text-justify"><b>For safety and security-</b></p>
+        <p class="mb-20 text-justify">We use personal data to verify accounts and user activity, as well as to promote safety and security, such as by monitoring fraud and investigating suspicious or potentially illegal activity or violations of our terms or policies.
+        </p>
+        <h5 class="title mb-20">2.Disclosure of user data</h5>
+
+        <p class="mb-20 text-justify">We may employ third-party companies and individuals due to the following reasons:</p>
+        <ul class="mb-20">
+            <li>To facilitate our services;</li>
+            <li>To provide the service on our behalf;</li>
+            <li>To perform website-related services; or</li>
+            <li>To assist us in analyzing how our website is used.</li>
+            <li>We want to inform our website users that these third parties have access to your personal information. The reason is to perform the tasks assigned to them on our behalfs such as website hosting, email services, marketing, sponsoring of sweepstakes, contests, and other promotions, auditing, fulfilling customer orders, data analytics, providing customer service, and conducting customer research and satisfaction surveys. However, they are obligated not to disclose or use the information for any other purpose.</li>
         </ul>
-        <p>Minors</p>
-        <p class="mb-20 text-justify">Our Platform is available only to persons who can form a legally binding contract under the Indian Contract Act, 1872. We do not intentionally collect Personal Information from children. If you are the parent or guardian and believe your child has provided us with Personal Information, please contact us at the address above to request deletion.</p>
-        <h5 class="title mb-20">Behavioural Advertising</h5>
-        <p class="mb-20 text-justify">As described above, we use your Personal Information to provide you with targeted advertisements or marketing communications we believe may be of interest to you. For example:</p>
-        <ul>
-            <li>We use Google Analytics to help us understand how our customers use the Site. You can read more about how Google uses your Personal Information here: https://www.google.com/intl/en/policies/privacy/. You can also opt-out of Google Analytics here: https://tools.google.com/dlpage/gaoptout.</li>
-            <li>We share information about your use of the Site, your purchases, and your interaction with our ads on other websites with our advertising partners. We collect and share some of this information directly with our advertising partners, and in some cases through the use of cookies or other similar technologies (which you may consent to, depending on your location).</li>
-            <li><b>Source of collection: </b> collected from you</li>
-        </ul>
+        <p class="mb-20 text-justify"><b>Security</b></p>
+        <p class="mb-20 text-justify">We value your trust in providing us your personal information, thus we are striving to use commercially acceptable means of protecting it. Unfortunately, the transmission of information over the internet is not completely secure and no method of electronic storage is 100% secure and reliable, and hence we cannot guarantee absolute security.</p>
+        <p class="mb-20 text-justify"> Once we receive your information, we utilize commercially reasonable procedures and security features to prevent unauthorized access.</p>
+        <p class="mb-20 text-justify"><b>Link to other sites</b></p>
+        <p class="mb-20 text-justify">We provide links to other websites for your convenience and information. These sites may have their own privacy policies in place. We are not responsible for the content of linked sites or any use of the sites. This privacy statement applies solely to data collected by us through our products and services. We encourage you to read the privacy policies of any third party before proceeding to use their websites, products, or services.</p>
+        <p class="mb-20 text-justify"><b>Children’s privacy</b></p>
+        <p class="mb-20 text-justify">Our services do not address anyone under the age of 13. We do not knowingly collect personally identifiable information from children under 13. In the case we discover that a child under 13 has provided us with personal information, we immediately delete this from our servers.</p>
+        <p class="mb-20 text-justify">If you are a parent or guardian and you are aware that your child has provided us with personal information, please contact us immediately so that we will be able to do necessary actions.
+        </p>
+        <p class="mb-20 text-justify"><b>Changes to the privacy policy</b></p>
+        <p class="mb-20 text-justify">We may periodically change this privacy statement to keep pace with new technologies, industry practices, and regulatory requirements, among other reasons. We expect most such changes to be minor. Any non-material changes will take effect immediately upon posting of an updated Privacy Statement.
+        </p>
+        <p class="mb-20 text-justify"><b>Contact Us</b></p>
+        <p class="mb-20 text-justify">If you have any questions or suggestions about our Privacy Policy, do not hesitate to contact us at ekaavastra@gmail.com. If you do not agree with our privacy policy, please refrain from using our website any further.</p>
+        <p class="mb-20 text-justify">(Due to current situations we are operating with limited staff, there might be delay in responses your patience is highly appreciated)</p>
+        <p class="mb-20 text-justify"> Please note that EKAA VASTRA or its employees will never send any payment link or ask for sensitive information like VPA-UPI related, User ID, PIN, Internet Banking passwords, Card details, CVV No, OTP, etc, through phone/SMS/email or any manner. pls be educated and never share such details to anyone.
+        </p>
+        <p class="mb-20 text-justify"><b>Disclaimer</b></p>
+        <p class="mb-20 text-justify">“EKAA VASTRA is a leading brand for women's apparels in India. The size and prominence of our brand unfortunately sometimes attracts the attention of unscrupulous individuals who falsely claim to provide goods and services under our popular brand name 'EKAA VASTRA' on rogue and fraudulent websites that bear no authorization or relation to the quality of our brand's products.</p>
+        <p class="mb-20 text-justify"> EKAA VASTRA has recently noticed that fraudulent, unauthorised and unscrupulous individuals in the market are using our registered trademarks and/or its variations with a view to tarnish the goodwill and reputation of our brand name 'EKAA VASTRA' and/or cheat the public at large. We accordingly wish to caution our beloved customers that EKAA VASTRA has neither appointed nor authorized any persons to act on its behalf or use our name, trademarks and logo and hereby advise individuals and other parties concerned to not fall prey to such fraudulent activities. Please note that the official website for our brand is “ekaavastra.com” only.”</p>
+        <p class="mb-20 text-justify">"While we routinely report and initiate the appropriate legal action against such abuses to law enforcement agencies, we would also request you to kindly report any suspicious promotional messages, fake websites and/or e-commerce portals who are intending to make a fast buck by misappropriating the familiar and trustworthy name of 'EKAA VASTRA'. You may report any such abuse at ekaavastra@gmail.com</p>
+        <h5 class="title mb-20">Disclaimer</h5>
+
+        <p class="mb-20 text-justify">We would like to inform our valued customers that our brand does not engage in making unsolicited or fake calls to customers. We have become aware of instances where individuals are receiving fraudulent calls impersonating our brand representatives.</p>
+        <p class="mb-20 text-justify">Please note that our official communication channels include our verified contact information, such as our official website, email addresses, and social media accounts. We urge you to exercise caution and not disclose any personal or financial information to anyone claiming to be associated with our brand over phone calls that you have not initiated.</p>
+        <p class="mb-20 text-justify">To ensure the security of your personal information, we recommend verifying the authenticity of any communication received by contacting our official customer support team directly through our verified channels. Your privacy and trust are of utmost importance to us, and we are taking necessary measures to address this issue.</p>
+        <p class="mb-20 text-justify">If you receive any suspicious calls claiming to be from our brand, we encourage you to report them to ekaavastra@gmail.com to prevent further fraudulent activities. We apologize for any inconvenience caused and thank you for your continued support and trust in our brand.</p>
+        <p class="mb-20 text-justify">Please stay vigilant and protect yourself from potential scams. Together, we can maintain a secure and trustworthy customer experience.</p>
+      
     </div>
 
 </section>
