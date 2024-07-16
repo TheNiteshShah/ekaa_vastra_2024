3e8a654ec7687684804d6047b283a6c51fcdd461
Nitesh Shah <niteshshah000@gmail.com>
2024-07-16 13:57:49 +0530

    updates

--- resources/views/frontend/refund_policy.blade.php
+++ resources/views/frontend/refund_policy.blade.php
@@ -23,26 +23,41 @@
 <!-- product tab start -->
 <section class="whish-list-section theme1 pb-70">
     <div class="container grid-wraper">
-        <h5 class="title mb-20">Return, Refund, and Exchange Policy</h5>
-        <p class="mb-20 text-justify">Thank you for choosing Ekaa Vastra for your shopping needs. We are dedicated to providing you with a seamless experience and ensuring your satisfaction with every purchase. Please review our Return, Refund, and Exchange Policy below to understand your options regarding your purchases.</p>
-        <h5 class="title mb-20">Return Policy:</h5>
-        <p class="mb-20 text-justify">We accept returns only for products received in a faulty, damaged, or incorrect condition. If you receive such a product, please contact us immediately. We will arrange for the faulty or damaged item to be picked up within 48 hours. Upon receiving the returned item, we will initiate a refund process, and you can expect the refunded amount to reflect in your bank account within 7-10 business days.</p>
-        <h5 class="title mb-20">Replacement & Exchange Policy:</h5>
-        <p class="mb-20 text-justify">We offer replacements and exchanges for products that are sent incorrectly, damaged, or faulty. If you wish to exchange the size, color, or the product itself, we will handle the return pick-up and redelivery process. A return pick-up will be scheduled within 48 hours, and you can expect the new delivery within 7 to 10 days. However, for correctly sent products, if you wish to exchange them, there will be an additional charge of ₹180 to cover the cost of pick-up and redelivery.</p>
-        <h5 class="title mb-20">Refund Policy:</h5>
-        <p class="mb-20 text-justify">If you receive a faulty or damaged product and wish to request a refund, the item must be in the same condition as you received it, unused, with tag, and in its original packaging. Contact us, and we will schedule a return pick-up within 48 hours. Once we receive the returned item, we will initiate the refund process, and you can expect the refunded amount to reflect in your bank account within 7 to 14 business days.</p>
-        <h5 class="title mb-20">Contact Us:</h5>
-        <p class="mb-20 text-justify">Call or WhatsApp us at +919636373743 between 10 am to 10 pm on business days.
-            Email us at ekaavastra@gmail.com between 10 am to 10 pm on business days.
-            Direct Message us on Instagram where we are available 24x7 for international shipping inquiries and support.
-            Order Processing Time:
-            We typically take 3 to 4 days to process an order. We aim to deliver within seven days, with a maximum of 10 days. In case of any delay, we offer express shipping options for your convenience.</p>
-        <h5 class="title mb-20">Damaged Items:</h5>
-        <p class="mb-20 text-justify">If you notice any damage upon receiving your order, please inspect it immediately and contact us. We will evaluate the issue and make it right by sending you a replacement product or issuing a complete refund.</p>
-        <h5 class="title mb-20">Exceptions/Non-Returnable Items:</h5>
-        <p class="mb-20 text-justify">Products received correctly and in the condition as mentioned on the website are not eligible for a refund. If you wish to replace a correctly sent product, there will be a fee of ₹180 to cover the cost of return pick-up and redelivery.</p>
-        <h5 class="title mb-20">International Shipping:</h5>
-        <p class="mb-20 text-justify">We provide international shipping, and the cost is calculated based on the parcel's weight and distance. The shipping cost will be charged to the customer. The return, replacement, and cancellation policies apply to international orders as well.</p>
+        <h5 class="title mb-20">Return, Refund, Exchange and Cancellation Policy</h5>
+        <p class="mb-20 text-justify">Thank you for choosing Ekaa Vastra for your shopping needs. We are dedicated to providing you with a seamless experience and ensuring your satisfaction with every purchase. Please review our Return, Refund, Exchange and Cancellation Policy below to understand your options regarding your purchases.</p>
+        <h5 class="title mb-20">Return & Refund Policy:</h5>
+        <p class="mb-20 text-justify"><b>Defective Products:</b> We accept return requests for defective products within 48 hours of delivery. Please raise a return request within this timeframe to receive a refund.</p>
+        <p class="mb-20 text-justify">NOTE - If you're unable to raise a return request from our website due to some unexpected reason, please email us at ekaavastra@gmail.com within 48 hours of delivery.</p>
+        <p class="mb-20 text-justify">Important: Please note that we will not be able to accept return requests for defective products post 48 hours of delivery. No exceptions will be made.</p>
+        <p class="mb-20 text-justify">Please act promptly to ensure a smooth return process.</p>
+        <p class="mb-20 text-justify"><b>Non-Defective Products:</b> Please note that we do not offer refunds for non-defective products. However, we can provide store credit equivalent to the order value, subject to the following conditions:</p>
+        <ul class="mb-20" style="list-style-type: circle;">
+            <li>Return requests must be made within 7 days of delivery.</li>
+            <li>You will have to pay the return shipping costs, which will be calculated based on the parcel weight and distance.</li>
+            <li>Once we receive the product in its original condition, we will issue store credit equivalent to the order value</li>
+        </ul>
+        <p class="mb-20 text-justify"><b>Credit Points:</b> The credit points will be reflected in your Ekaa Vastra wallet within 4-5 days after we receive the returned products. You can read more about credit points here</p>
+        <h5 class="title mb-20">Size Exchange Policy:</h5>
+        <p class="mb-20 text-justify">To request a size replacement:</p>
+        <ul class="mb-20" style="list-style-type: circle;">
+            <li>Go to your Recent Orders page and raise a return/exchange request for the selected item.</li>
+            <li>Our team will review your request and contact you if necessary.</li>
+        </ul>
+        <p class="mb-20 text-justify">Please note:</p>
+        <p class="mb-10 text-justify">Size exchange is subject to product availability. If the desired size is not available, we cannot process the exchange.</p>
+        <ul class="mb-20" style="list-style-type: circle;">
+        <li >In this case, you can return the product and receive store credits.</li>
+        <li >If your exchange request is accepted, you will be required to pay the reverse shipping fee in the order section.</li>
+        <li >Once paid, our delivery agent will pick up the exchangeable product(s) within 7-9 days.</li>
+        <li >After receiving your parcel, we will dispatch the required size within 5-7 days.</li>
+        </ul>
+       
+        <p class="mb-20 text-justify">We aim to make size exchanges as smooth as possible, but please be aware of the dependencies on product availability."</p>
+        <h5 class="title mb-20">Cancellation Policy:</h5>
+        <p class="mb-20 text-justify">You can cancel your order online until it is <b>dispatched</b>. Simply go to 'Your Orders', select the order, and click 'Cancel My Order' before the status shows <b>'Dispatched'</b>. No further action is required.</p>
+        <p class="mb-20 text-justify">For prepaid orders, the amount will be refunded to your original payment method or transferred to your bank account, typically within 3-5 business days, after cancellation.</p>
+        
+       
         <p class="mb-20 text-justify">If you have any further questions or concerns regarding our policies, please do not hesitate to contact us using the provided methods. Thank you for shopping with us!</p>
     </div>
 
