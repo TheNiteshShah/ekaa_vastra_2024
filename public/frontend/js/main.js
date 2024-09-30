4e0104a6baeb945efc036c4ffaa91486c7fd21d7
Nitesh Shah <niteshshah000@gmail.com>
2024-09-30 18:25:12 +0530

    updates

--- public/frontend/js/main.js
+++ public/frontend/js/main.js
@@ -199,7 +199,7 @@
     }, {
       breakpoint: 576,
       settings: {
-        slidesToShow: 1,
+        slidesToShow: 2,
         slidesToScroll: 1,
         arrows: false,
         autoplay: true
