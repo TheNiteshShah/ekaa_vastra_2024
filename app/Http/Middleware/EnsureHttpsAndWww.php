400e597e3e30d130c6f3cd8907167d239bbc096c
Nitesh Shah <niteshshah000@gmail.com>
2025-02-12 23:32:10 +0530

    updates

--- app/Http/Middleware/EnsureHttpsAndWww.php
+++ app/Http/Middleware/EnsureHttpsAndWww.php
@@ -9,7 +9,7 @@ class EnsureHttpsAndWww
 {
     /**
      * Handle an incoming request.
-     *
+     * 
      * @param  \Illuminate\Http\Request  $request
      * @param  \Closure  $next
      * @return mixed
