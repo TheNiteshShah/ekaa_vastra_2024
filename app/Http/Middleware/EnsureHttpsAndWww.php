80607bd794748e9a303f00bd3bca1e3ed0f64292
Nitesh Shah <niteshshah000@gmail.com>
2025-02-12 23:30:26 +0530

    url correct redirection

--- app/Http/Middleware/EnsureHttpsAndWww.php
+++ app/Http/Middleware/EnsureHttpsAndWww.php
@@ -1,33 +1,16 @@
-<?php
-
-namespace App\Http\Middleware;
-
-use Closure;
-use Illuminate\Http\Request;
-
-class EnsureHttpsAndWww
+public function handle(Request $request, Closure $next)
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
-     */
-    public function handle(Request $request, Closure $next)
-    {
-        $host = $request->getHost();
-        $scheme = $request->getScheme();
+    $host = $request->getHost();
+    $scheme = $request->getScheme();
 
-        // Check if the scheme is not HTTPS or if the host does not start with 'www.'
-        if ($scheme !== 'https' || strpos($host, 'www.') !== 0) {
-            // Construct the new URL with 'https://www.'
-            $newHost = (strpos($host, 'www.') === 0) ? $host : 'www.' . $host;
-            $newUrl = 'https://' . $newHost . $request->getRequestUri();
+    // Ensure HTTPS and "www."
+    if ($scheme !== 'https' || substr($host, 0, 4) !== 'www.') {
+        // Ensure 'www.' is added to the host
+        $newHost = (substr($host, 0, 4) !== 'www.') ? 'www.' . $host : $host;
+        $newUrl = 'https://' . $newHost . $request->getRequestUri();
 
-            return redirect($newUrl, 301); // Permanent redirect
-        }
-
-        return $next($request);
+        return redirect()->to($newUrl, 301); // Permanent Redirect
     }
+
+    return $next($request);
 }
