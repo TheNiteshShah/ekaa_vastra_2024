f4a92562a1ced3825a56165402a88802ffc0afc4
Nitesh Shah <niteshshah000@gmail.com>
2025-02-25 19:53:41 +0530

    updates

--- resources/views/frontend/checkout.blade.php
+++ resources/views/frontend/checkout.blade.php
@@ -94,8 +94,7 @@
     .table-responsive.wdfqw::-webkit-scrollbar {
         width: 5px;
         height: 8px;
-        background-color: #2caf4e;
-
+        background-color: #292929;
     }
 
     .border-class1.active {
